3d7de11f2d533ce0b68d7bae351e6814f6df5a55
zhaijf <zhaijunfeng2012@gmail.com>
2020-10-26 10:46:06 +0800

    del

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,23 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
+#[test]
+fn check_answer_validity() {
+    println!("121212");
+}
+
+
 fn main() {
     let args = Cli::from_args();
     let pattern = std::env::args().nth(1).expect("no pattern given");
     let path = std::env::args().nth(2).expect("no path given");
+
+    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
 
     let args = Cli {
         pattern: pattern,

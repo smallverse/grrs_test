265f2d93e43496da146d191404da12ad5c52cba8
zhaijf <zhaijunfeng2012@gmail.com>
2020-10-27 14:54:34 +0800

    m

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+
+#![allow(unused_variables)]
+fn main() {
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
+
+}
